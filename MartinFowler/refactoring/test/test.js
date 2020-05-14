98984d8efa8e0b8dc4809d73a0d46ce85cd4e515
eltonplima <eltonplima@gmail.com>
2020-05-14 08:24:22 +0100

    Add some tests before start refactoring

---
+++ MartinFowler/refactoring/test/test.js
@@ -0,0 +1,105 @@
+expect = require('chai').expect;
+statement = require('../statement');
+
+
+describe('Verify bill report', () => {
+    let plays;
+    beforeEach(() => {
+        plays = {
+            "hamlet": {
+                "name": "Hamlet",
+                "type": "tragedy"
+            },
+            "as-like": {
+                "name": "As You Like It",
+                "type": "comedy"
+            },
+            "othello": {
+                "name": "Othello",
+                "type": "tragedy"
+            },
+            "i-dont-know": {
+                "name": "I don't know",
+                "type": "horror"
+            },
+        }
+    });
+    describe('When invoice has', () => {
+        it('one unknown performance with audience of 100', function () {
+            const invoices = {
+                "customer": "BigCo",
+                "performances": [
+                    {
+                        "playID": "i-dont-know",
+                        "audience": 100
+                    }
+                ]
+            }
+            expect(() => statement(invoices, plays)).to.throw(Error, "unknown type: horror");
+        });
+        it('one tragedy performance with audience greater than 30', function () {
+            const invoices = {
+                "customer": "BigCo",
+                "performances": [
+                    {
+                        "playID": "hamlet",
+                        "audience": 55
+                    }
+                ]
+            }
+            const value = statement(invoices, plays);
+            expect(value).to.equal("Statement for BigCo\n" +
+                "  Hamlet: $650.00(55 seats)\n" +
+                "Amount owed is $650.00\n" +
+                "You earned 25 credits\n");
+        });
+        it('one tragedy performance with audience less than 30', function () {
+            const invoices = {
+                "customer": "BigCo",
+                "performances": [
+                    {
+                        "playID": "hamlet",
+                        "audience": 10
+                    }
+                ]
+            }
+            const value = statement(invoices, plays);
+            expect(value).to.equal("Statement for BigCo\n" +
+                "  Hamlet: $400.00(10 seats)\n" +
+                "Amount owed is $400.00\n" +
+                "You earned 0 credits\n");
+        });
+        it('one comedy performance with audience greater than 20', function () {
+            const invoices = {
+                "customer": "BigCo",
+                "performances": [
+                    {
+                        "playID": "as-like",
+                        "audience": 55
+                    }
+                ]
+            }
+            const value = statement(invoices, plays);
+            expect(value).to.equal("Statement for BigCo\n" +
+                "  As You Like It: $740.00(55 seats)\n" +
+                "Amount owed is $740.00\n" +
+                "You earned 36 credits\n");
+        });
+        it('one comedy performance with audience less than 20', function () {
+            const invoices = {
+                "customer": "BigCo",
+                "performances": [
+                    {
+                        "playID": "as-like",
+                        "audience": 10
+                    }
+                ]
+            }
+            const value = statement(invoices, plays);
+            expect(value).to.equal("Statement for BigCo\n" +
+                "  As You Like It: $330.00(10 seats)\n" +
+                "Amount owed is $330.00\n" +
+                "You earned 2 credits\n");
+        });
+    });
+})
