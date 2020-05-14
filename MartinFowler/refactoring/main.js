ee34a0d991cc29dca8e96a1a40cf9b6a9496b1da
eltonplima <eltonplima@gmail.com>
2020-05-14 08:24:05 +0100

    Rename variable

--- MartinFowler/refactoring/main.js
+++ MartinFowler/refactoring/main.js
@@ -2,7 +2,7 @@ statement = require('./statement');
 
 const fs = require('fs');
 
-let invoices = JSON.parse(fs.readFileSync('data/invoices.json'))[0];
+let invoice = JSON.parse(fs.readFileSync('data/invoices.json'))[0];
 let plays = JSON.parse(fs.readFileSync('data/plays.json'));
 
-console.log(statement(invoices, plays));
+console.log(statement(invoice, plays));
