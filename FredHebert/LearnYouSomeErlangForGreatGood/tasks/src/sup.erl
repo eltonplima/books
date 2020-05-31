3e730e9fa4760bf23f3520d49a95d7098eb5cde3
eltonplima <eltonplima@gmail.com>
2020-05-31 09:55:41 +0100

    Add supervisor

---
+++ FredHebert/LearnYouSomeErlangForGreatGood/tasks/src/sup.erl
@@ -0,0 +1,33 @@
+%%%-------------------------------------------------------------------
+%%% @author eltonplima
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 31. mai 2020 09:39
+%%%-------------------------------------------------------------------
+-module(sup).
+-author("eltonplima").
+
+%% API
+-export([start/2, start_link/2, init/1, loop/1]).
+
+start(Mod, Args) ->
+  spawn(?MODULE, init, [{Mod, Args}]).
+
+start_link(Mod, Args) ->
+  spawn_link(?MODULE, init, [{Mod, Args}]).
+
+init({Mod, Args}) ->
+  process_flag(trap_exit, true),
+  loop({Mod, start_link, Args}).
+
+loop({M, F, A}) ->
+  Pid = apply(M, F, A),
+  receive
+    {'EXIT', _From, shutdown} ->
+      exit(shutdown);  % we will kill the child too
+    {'EXIT', Pid, Reason} ->
+      io:format("Process ~p exited for reason ~p~n", [Pid, Reason]),
+      loop({M, F, A})
+  end.
