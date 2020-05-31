62dd004e1e92f604cdb393842035a0c602794752
eltonplima <eltonplima@gmail.com>
2020-05-31 09:31:45 +0100

    Add function start_link
    * This function is part of the public interface

--- FredHebert/LearnYouSomeErlangForGreatGood/tasks/src/evserv.erl
+++ FredHebert/LearnYouSomeErlangForGreatGood/tasks/src/evserv.erl
@@ -74,6 +74,10 @@ start() ->
   register(?MODULE, Pid=spawn(?MODULE, init, [])),
   Pid.
 
+start_link() ->
+  register(?MODULE, Pid = spawn_link(?MODULE, init, [])),
+  Pid.
+
 valid_datetime({Date, Time}) ->
   try
     calendar:valid_date(Date) andalso valid_time(Time)
