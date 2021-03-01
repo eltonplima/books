8112d047f11b4db20762586b88b95b9dcb1adfce
eltonplima <me@eltonplima.dev>
2021-03-01 22:21:19 +0000

    Implementation of challenges
    * lines_lengths!/1
    * longest_line_length!/1
    * longest_line!/1
    * words_per_line!/1

--- Saša Jurić/elixir_in_action/apps/chapter3/lib/enum_stream_practices.ex
+++ Saša Jurić/elixir_in_action/apps/chapter3/lib/enum_stream_practices.ex
@@ -1,9 +1,30 @@
 defmodule EnumStreamPractices do
   @moduledoc false
 
-  def large_lines!(path, length \\ 80) do
+  def large_lines!(path, line_length \\ 80) do
     File.stream!(path)
     |> Stream.map(&String.replace(&1, "\n", ""))
-    |> Enum.filter(&(String.length(&1) > length))
+    |> Enum.filter(&(String.length(&1) > line_length))
+  end
+
+  def lines_lengths!(path) do
+    Enum.map(large_lines!(path, 0), fn string -> String.length(string) end)
+  end
+
+  def longest_line_length!(path) do
+    large_lines!(path, 0)
+    |> Enum.map(&String.length(&1))
+    |> Enum.max()
+  end
+
+  def longest_line!(path) do
+    large_lines!(path, 0)
+    |> Enum.max(&(String.length(&1) > String.length(&2)))
+  end
+
+  def words_per_line!(path) do
+    large_lines!(path, 0)
+    |> Enum.map(&String.split(&1))
+    |> Enum.map(&Enum.count(&1))
   end
 end
--- Saša Jurić/elixir_in_action/apps/chapter3/test/enum_stream_practices_test.exs
+++ Saša Jurić/elixir_in_action/apps/chapter3/test/enum_stream_practices_test.exs
@@ -7,17 +7,69 @@ defmodule EnumStreamPracticesTest do
 
   doctest EnumStreamPractices
 
-  test "get lines with length greater than the default 80 columns" do
-    assert EnumStreamPractices.large_lines!("test/fixtures/example.txt") == [
+  setup_all do
+    file_content = "This is a sample paragraph.
+A
+Simple
+Word
+Per
+Line
+AGreaterWord
+This is a really long, long, long line of text to test. With more than 80 columns!"
+    example_file_1 = "test/example.txt"
+    File.write!(example_file_1, file_content, [:write, {:encoding, :utf8}])
+    on_exit(fn -> File.rm!(example_file_1) end)
+    {:ok, text_file_path: example_file_1}
+  end
+
+  test "get lines with length greater than the default 80 columns", state do
+    IO.inspect(state[:text_file_path])
+
+    assert EnumStreamPractices.large_lines!(state[:text_file_path]) == [
              "This is a really long, long, long line of text to test. With more than 80 columns!"
            ]
   end
 
-  test "get lines with length greater than the custom 10 columns" do
-    assert EnumStreamPractices.large_lines!("test/fixtures/example.txt", 10) == [
+  test "get lines with length greater than the custom 10 columns", state do
+    assert EnumStreamPractices.large_lines!(state[:text_file_path], 10) == [
              "This is a sample paragraph.",
              "AGreaterWord",
              "This is a really long, long, long line of text to test. With more than 80 columns!"
+           ]
+  end
+
+  test "get lines lengths", state do
+    assert EnumStreamPractices.lines_lengths!(state[:text_file_path]) == [
+             27,
+             1,
+             6,
+             4,
+             3,
+             4,
+             12,
+             82
+           ]
+  end
+
+  test "get longest line length", state do
+    assert EnumStreamPractices.longest_line_length!(state[:text_file_path]) == 82
+  end
+
+  test "get longest line", state do
+    assert EnumStreamPractices.longest_line!(state[:text_file_path]) ==
+             "This is a really long, long, long line of text to test. With more than 80 columns!"
+  end
+
+  test "words per line", state do
+    assert EnumStreamPractices.words_per_line!(state[:text_file_path]) == [
+             5,
+             1,
+             1,
+             1,
+             1,
+             1,
+             1,
+             17
            ]
   end
 end
