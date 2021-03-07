9a6afdb804026d3677d6a8e6520cbab76f197039
eltonplima <me@eltonplima.dev>
2021-03-07 15:52:47 +0000

    Add function new to the module TodoEntry

--- Saša Jurić/elixir_in_action/apps/chapter4/lib/todo_list/todo_entry.ex
+++ Saša Jurić/elixir_in_action/apps/chapter4/lib/todo_list/todo_entry.ex
@@ -1,4 +1,8 @@
 defmodule Chapter4.TodoList.TodoEntry do
   @moduledoc false
   defstruct id: nil, date: nil, title: nil
+
+  def new(%{date: date, title: title}) do
+    %__MODULE__{date: date, title: title}
+  end
 end
---
+++ Saša Jurić/elixir_in_action/apps/chapter4/test/todo_list/todo_entry_test.exs
@@ -0,0 +1,18 @@
+defmodule Chapter4.TodoList.TodoEntryTest do
+  use ExUnit.Case
+
+  alias Chapter4.TodoList.TodoEntry
+
+  @moduletag :capture_log
+
+  doctest TodoEntry
+
+  test "module exists" do
+    assert is_list(TodoEntry.module_info())
+  end
+
+  test "new method" do
+    todo_entry = TodoEntry.new(%{date: ~D[2021-03-07], title: "Shopping"})
+    assert todo_entry == %TodoEntry{date: ~D[2021-03-07], title: "Shopping"}
+  end
+end
