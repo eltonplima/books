db7061cc7c1fc0b3cba84fb73dfbac9962861921
eltonplima <me@eltonplima.dev>
2021-05-14 15:17:18 +0100

    Add ssl in extra_applications
    * I do this because of the warnings of the mix about ssl

--- Darin Wilson and Eric Jonsson/programming_ecto/wmecto-code/code/mix.exs
+++ Darin Wilson and Eric Jonsson/programming_ecto/wmecto-code/code/mix.exs
@@ -23,7 +23,7 @@ defmodule MusicDB.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :ssl],
       mod: {MusicDB.Application, []}
     ]
   end
